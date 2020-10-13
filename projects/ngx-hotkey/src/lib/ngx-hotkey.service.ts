3f1a5b022335b37af242a79669f36908e32abca8
Danilo Peres <danilo_meirelles@hotmail.com>
2020-10-13 23:07:09 +0200

    Fire the action only once during keydown

--- projects/ngx-hotkey/src/lib/ngx-hotkey.service.ts
+++ projects/ngx-hotkey/src/lib/ngx-hotkey.service.ts
@@ -6,9 +6,11 @@ import Hotkey from './hotkey.model';
 })
 export class NgxHotkeyService {
     private hotkeys: Hotkey[] = [];
+    private lastHotkeyFired: Hotkey;
 
     constructor() {
-        document.addEventListener('keydown', this.checkKeyUp.bind(this), false);
+        document.addEventListener('keydown', this.checkKeyDown.bind(this), false);
+        document.addEventListener('keyup', this.onKeyUp.bind(this), false);
     }
 
     add(shortcut: string, callback: (event: KeyboardEvent) => boolean) {
@@ -19,13 +21,20 @@ export class NgxHotkeyService {
         this.hotkeys = this.hotkeys.filter(x => x.shortcut !== shortcut);
     }
 
-    private checkKeyUp(event: KeyboardEvent): boolean {
+    private checkKeyDown(event: KeyboardEvent): boolean {
         for (let i = 0; i < this.hotkeys.length; i++) {
             const hotkey = this.hotkeys[i];
-            if (this.checkHotKey(event, hotkey))
+            if (this.checkHotKey(event, hotkey) && !this.isTheSameLastHotkeyFired(hotkey)) {
+                this.lastHotkeyFired = hotkey;
                 return hotkey.callback(event);
+            }
         }
 
+        return false;
+    }
+
+    private onKeyUp(): boolean {
+        this.lastHotkeyFired = null;
         return false;
     }
 
@@ -35,5 +44,9 @@ export class NgxHotkeyService {
             && event.metaKey === hotkey.metaKey
             && event.shiftKey === hotkey.shiftKey
             && event.key.toLowerCase() === hotkey.key;
+    }
+
+    private isTheSameLastHotkeyFired(hotkey: Hotkey) {
+        return hotkey === this.lastHotkeyFired;
     }
 }
